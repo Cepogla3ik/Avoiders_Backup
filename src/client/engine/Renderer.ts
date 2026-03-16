82bbbef56816f046b72cc629cc6477281a85c9fd
Cepoglazik <cpo.gla3ik@gmail.com>
2026-03-16 18:00:45 +0200

    Update from Android

--- src/client/engine/Renderer.ts
+++ src/client/engine/Renderer.ts
@@ -1,9 +1,16 @@
+import GameWorld from "@server/GameWorld/GameWorld";
+
 export default class Renderer {
   constructor(canvas: HTMLCanvasElement) {
     canvas.getContext("2d");
   }
   // TODO
 
-  start() {}
-  stop() {}
+  start() {
+    const gameWorld = new GameWorld();
+    console.log("CanvasGame launched!", gameWorld);
+  }
+  stop() {
+    console.log("CanvasGame stopped");
+  }
 }
